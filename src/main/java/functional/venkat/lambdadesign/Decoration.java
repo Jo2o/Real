1735461919c734fefdbd60c05a9cad9c6829b20c
JozefMikus <jozo.mikus@gmail.com>
2021-12-01 09:08:34 +0100

    added python module

---
+++ src/main/java/functional/venkat/lambdadesign/Decoration.java
@@ -0,0 +1,22 @@
+package functional.venkat.lambdadesign;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.function.Function;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Decoration {   // multiple delegates - chain them to add behavior
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
