f163b18e84286be95bceab0a4a41f80c5b119a23
JozefMikus <jozef.mikus@kyndryl.com>
2021-12-03 08:59:00 +0100

    -- fluent mailer

---
+++ src/main/java/functional/venkat/fluentinterfaces/FluentMailer.java
@@ -0,0 +1,44 @@
+package functional.venkat.fluentinterfaces;
+
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@Accessors
+class FluentMailer {
+    private String from;
+    private String to;
+    private String subject;
+    private String body;
+
+    private FluentMailer() {
+    }
+
+    public FluentMailer from(final String address) {
+        this.from = address;
+        return this;
+    }
+
+    public FluentMailer to(final String address) {
+        this.to = address;
+        return this;
+    }
+
+    public FluentMailer subject(final String line) {
+        this.subject = line;
+        return this;
+    }
+
+    public FluentMailer body(final String message) {
+        this.body = message;
+        return this;
+    }
+
+    public static void send(final Consumer<FluentMailer> block) {
+        FluentMailer fluentMailer = new FluentMailer();
+        block.accept(fluentMailer);     // here it is as setter
+        log.info("Sending FluentMailer...");
+    }
+}
--- src/main/java/functional/venkat/fluentinterfaces/Main.java
+++ src/main/java/functional/venkat/fluentinterfaces/Main.java
@@ -1,5 +1,85 @@
 package functional.venkat.fluentinterfaces;
 
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Builder
 public class Main {
-    
+    public static void main(String[] args) {
+        new Main().test();
+    }
+
+    private void test() {
+
+        Mailer mailer = new Mailer();                         // lots of duplication
+        mailer.from("build@agiledeveloper.com");
+        mailer.to("venkats@agiledeveloper.com");
+        mailer.subject("build notification");
+        mailer.body("...your code sucks...");
+        mailer.send();
+
+        new MailerBuilder()                                   // still new keyword is here and may be stored elsewhere preventing GC
+                .from("build@agiledeveloper.com")
+                .to("venkats@agiledeveloper.com")
+                .subject("build notification")
+                .body("...your code sucks less...")
+                .send();
+
+        FluentMailer.send(m -> m.from("build@agiledeveloper.com") // got rid of new with (static send) + (private constructor) which creates new instance inside
+                .to("venkats@agiledeveloper.com")                 // m is not defined here, it is captured/closure inside send() and then setters are carried out
+                .subject("build notification")                    // then actions in send are done
+                .body("...much better..."));
+    }
+
+    class Mailer {
+        private String from;
+        private String to;
+        private String subject;
+        private String body;
+
+        public void from(final String address) {
+            this.from = address;
+        };
+        public void to(final String address) {
+            this.to = address;
+        };
+        public void subject(final String line) {
+            this.subject = line;
+        };
+        public void body(final String message) {
+            this.body = message;
+        };
+        public void send() {
+            log.info("Sending Mailer...");
+        };
+    }
+
+    class MailerBuilder {
+        private String from;
+        private String to;
+        private String subject;
+        private String body;
+
+        public MailerBuilder from(final String address) {
+            this.from = address;
+            return this;
+        };
+        public MailerBuilder to(final String address) {
+            this.to = address;
+            return this;
+        };
+        public MailerBuilder subject(final String line) {
+            this.subject = line;
+            return this;
+        };
+        public MailerBuilder body(final String message) {
+            this.body = message;
+            return this;
+        };
+        public void send() {
+            log.info("Sending MailerBuilder...");
+        };
+    }
+
 }
