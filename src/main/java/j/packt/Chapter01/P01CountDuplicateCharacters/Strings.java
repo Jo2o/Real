e6b5936cc55a4c9ecbac29e1859991c01ad07da0
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-18 11:55:33 +0100

    -- count duplicate characters & unicode

--- src/main/java/j/packt/Chapter01/P01CountDuplicateCharacters/Strings.java
+++ src/main/java/j/packt/Chapter01/P01CountDuplicateCharacters/Strings.java
@@ -12,49 +12,36 @@ public final class Strings {
     }
 
     public static Map<Character, Integer> countDuplicateCharactersV1(String str) {
-
         if (str == null || str.isBlank()) {
-            // or throw IllegalArgumentException
-            return Collections.emptyMap();
+            return Collections.emptyMap(); // or throw IllegalArgumentException
         }
-
-        Map<Character, Integer> result = new HashMap<>();                
-
-        // or use for(char ch: str.toCharArray()) { ... }
-        for (int i = 0; i < str.length(); i++) {
+        Map<Character, Integer> result = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) { // or use for(char ch: str.toCharArray()) { ... }
             char ch = str.charAt(i);
-
             result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
         }
-
         return result;
     }
 
     public static Map<String, Integer> countDuplicateCharactersVCP1(String str) {
-
         if (str == null || str.isEmpty()) {
-            // or throw IllegalArgumentException            
-            return Collections.emptyMap();
+            return Collections.emptyMap(); // or throw IllegalArgumentException
         }
 
         Map<String, Integer> result = new HashMap<>();
-
         for (int i = 0; i < str.length(); i++) {
-            
             /*
             String ch = String.valueOf(Character.toChars(str.codePointAt(i)));
             if (i < str.length() - 1 && str.codePointCount(i, i + 2) == 1) {
                 i++;
             }
             */
-            
             // or, like this (this code produce the same result as the commented code above
             int cp = str.codePointAt(i);
             String ch = String.valueOf(Character.toChars(cp));
             if(Character.charCount(cp) == 2) { // 2 means a surrogate pair
                 i++;
             }
-
             result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
         }
 
@@ -62,30 +49,20 @@ public final class Strings {
     }
 
     public static Map<Character, Long> countDuplicateCharactersV2(String str) {
-
         if (str == null || str.isBlank()) {
-            // or throw IllegalArgumentException
-            return Collections.emptyMap();
+            return Collections.emptyMap(); // or throw IllegalArgumentException
         }
-
-        Map<Character, Long> result = str.chars()
+        return str.chars()
                 .mapToObj(c -> (char) c)
                 .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
-
-        return result;
     }
 
     public static Map<String, Long> countDuplicateCharactersVCP2(String str) {
-
         if (str == null || str.isBlank()) {
-            // or throw IllegalArgumentException
-            return Collections.emptyMap();
+            return Collections.emptyMap(); // or throw IllegalArgumentException
         }
-
-        Map<String, Long> result = str.codePoints()
+        return str.codePoints()
                 .mapToObj(c -> String.valueOf(Character.toChars(c)))
                 .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
-
-        return result;
     }
 }
